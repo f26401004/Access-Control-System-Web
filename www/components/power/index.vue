9809e170fc012a4a5b9cfd57587f40bf3a29e50c
f26401004 <f26401004@gmail.com>
2018-08-08 11:12:59 +0800

    Complete the simple structure of power page.

--- www/components/power/index.vue
+++ www/components/power/index.vue
@@ -1,15 +1,45 @@
 <template>
     <div>
-        <h1> Power Component </h1>
+        <section>
+            <h2> 設定門禁週期 </h2> <br/>
+            <label> 門禁開啟 </label>
+            <select> 
+                <option v-for="(item, index) in time"> {{item}}  </option>
+            </select> <br/>
+            <label> 門禁關閉 </label>
+            <select> 
+                <option v-for="(item, index) in time"> {{item}}  </option>
+            </select> <br/>
+            <button> 設定 </button>
+        </section>
+        <section>
+            <h2> 設定門禁 </h2> <br/>
+            <button>強制開啟</button> <br/>
+            <button>強制關閉</button>
+        </section>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'power'
+    name: 'power',
+    data () {
+        return {
+            time: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', 
+                '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', 
+                '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', 
+                '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', 
+            ]
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+h2 {
+    color: antiquewhite;
+}
+label {
+    color: antiquewhite;
+}
 </style>
